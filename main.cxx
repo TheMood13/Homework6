f2bebb35f92e587da2ee4c0a4597a7928eb9702d
Mhd Mahmoud Yehya <mahmoudyehya11@hotmail.com>
2023-10-01 15:25:41 +0200

    main.cxx file fixed by changing line 14 to account var8 as a string, and all tests were done. The tests showed that when running the file the order of the input variables mattered, that there could only be 2 variables, and that the file name length mattered.

--- main.cxx
+++ main.cxx
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 int main(int var0, char *var1[]) {
   if (var0 == 3) {
     std::string var2{var1[0]};
@@ -8,7 +9,7 @@ int main(int var0, char *var1[]) {
     auto var5{std::atoi(var1[2])};
     auto var6{0};
     auto var7{0};
-    auto var8{var1[1]};
+    std::string var8{var1[1]};
     while (true) {
       var6 += var8[var7++];
       if (var7 >= static_cast<int>(var8.size())) {
